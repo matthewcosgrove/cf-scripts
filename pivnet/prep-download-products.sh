639fa423cbae757cdd90881d2ed38b98efd0a2e5
matthewcosgrove <mcosgrove@pivotal.io>
2016-08-25 09:29:36 +0100

    Fixed issue with version output

--- pivnet/prep-download-products.sh
+++ pivnet/prep-download-products.sh
@@ -4,14 +4,11 @@ echo "Preparing pre-requisites for downloading products.."
 
 if hash jq &>/dev/null
 	then
-		jq_version=$(echo jq --version)
+		jq_version=$(jq --version)
     	echo "Skipping jq install. Already installed: $jq_version"
-	else		
+	else
+		echo "jq install required.."
     	sudo apt-get update
 		sudo apt-get install jq
 		jq --version
 fi
-
-
-
-
